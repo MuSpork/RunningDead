eb789a3594b7a937ea9384ef4eac54ecb4912275
Gio Santos <d.santos3@hotmail.com>
2015-04-20 10:58:52 +1200

    LocationListener Functionality added

--- app/src/main/java/net/ricky/runningdead/PlayMap.java
+++ app/src/main/java/net/ricky/runningdead/PlayMap.java
@@ -1,17 +1,30 @@
 package net.ricky.runningdead;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.provider.Settings;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class PlayMap extends ActionBarActivity {
+
+public class PlayMap extends ActionBarActivity implements LocationListener{
+    GoogleMap googlemap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_map);
+        initMap();
     }
 
 
@@ -36,4 +49,48 @@ public class PlayMap extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onLocationChanged(Location location) {
+
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    public void onProviderDisabled(String provider) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("GPS is disabled");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Enable GPS",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent startGps = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(startGps);
+            }
+        });
+        builder.setNegativeButton("Leave GPS off", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+
+    private void initMap(){
+        final SupportMapFragment mf = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        googlemap = mf.getMap();
+        googlemap.setMyLocationEnabled(true);
+        googlemap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
 }
