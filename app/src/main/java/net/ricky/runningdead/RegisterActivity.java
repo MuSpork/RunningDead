58c5e7b9eba7cc2d37d3b1b01e34b507af4ab708
Gio Santos <d.santos3@hotmail.com>
2015-05-06 21:58:21 +1200

    fixed gender to only 3 values

--- app/src/main/java/net/ricky/runningdead/RegisterActivity.java
+++ app/src/main/java/net/ricky/runningdead/RegisterActivity.java
@@ -6,7 +6,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 //needs to implement onClickListener for the Done button
 
@@ -19,7 +23,32 @@ public class RegisterActivity extends Activity {
         Intent intent = getIntent();
         EditText userNameTextView = (EditText) findViewById(R.id.userName);
         userNameTextView.setText(intent.getStringExtra("Username"));
+
+        Spinner spinner = (Spinner) findViewById(R.id.gender);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Gender, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
+
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+        //...
+
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            // parent.getItemAtPosition(pos)
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
+    }
+
 
 
     @Override
