8613310cdf667abde4ee84f989618f38e8879338
Ricky <rickx342@gmail.com>
2015-05-07 00:49:35 +1200

    Register is working

--- app/src/main/java/net/ricky/runningdead/RegisterActivity.java
+++ app/src/main/java/net/ricky/runningdead/RegisterActivity.java
@@ -2,29 +2,45 @@ package net.ricky.runningdead;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+
+import android.widget.Button;
+
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 //needs to implement onClickListener for the Done button
 
-public class RegisterActivity extends Activity {
-
+public class RegisterActivity extends Activity implements View.OnClickListener {
+    EditText userNameTextView;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Intent intent = getIntent();
-        EditText userNameTextView = (EditText) findViewById(R.id.userName);
+        userNameTextView = (EditText) findViewById(R.id.userName);
         userNameTextView.setText(intent.getStringExtra("Username"));
 
-        Spinner spinner = (Spinner) findViewById(R.id.gender);
+        spinner = (Spinner) findViewById(R.id.genderDropDown);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.Gender, android.R.layout.simple_spinner_item);
@@ -33,6 +49,9 @@ public class RegisterActivity extends Activity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+
+        Button doneButton = (Button) findViewById(R.id.done_button);
+        doneButton.setOnClickListener(this);
     }
 
     public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
@@ -71,5 +90,92 @@ public class RegisterActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void launchRegoSuccess(){
+        Intent intent = new Intent(this,RegisterSuccessful.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Register register = new Register();
+        register.execute(buildParameter());
+    }
+
+    public EditText[] findViewsById(){
+        EditText[] edits = new EditText[5];
+        edits[0] = (EditText) findViewById(R.id.firstName);
+        edits[1] = (EditText) findViewById(R.id.lastName);
+        edits[2] = (EditText) findViewById(R.id.emailAddress);
+        edits[3] = (EditText) findViewById(R.id.password);
+        edits[4] = (EditText) findViewById(R.id.dateOfBirth);
+
+        return edits;
+    }
+
+    public String[] getTextsFromViews(EditText[] edits){
+        ArrayList<String> strings = new ArrayList<String>();
+        for(EditText texts : edits){
+            strings.add(texts.getText().toString());
+        }
+        System.out.println("ArrayList = "+strings.toString());
+
+        return Arrays.copyOf(strings.toArray(),strings.toArray().length,String[].class);
+    }
+
+    public String buildParameter(){
+        String[] texts = getTextsFromViews(findViewsById());
+        String body = "firstName="+texts[0]+"&" +
+                "lastName="+texts[1]+"&" +
+                "emailAddress="+texts[2]+"&" +
+                "username="+userNameTextView.getText().toString()+"&" +
+                "password="+texts[3]+"&" +
+                "gender="+spinner.getSelectedItem().toString()+"&"+
+                "dateOfBirth="+texts[4];
+        System.out.println("Inside buildParamater = "+body);
+        return body;
+    }
+
+    public class Register extends AsyncTask<String,Void,Boolean>{
+        String request = "http://rickx.ddns.net/RegisterServlet";
+        URL url = null;
+        HttpURLConnection connection = null;
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            try {
+                url = new URL(request);
+                connection = (HttpURLConnection) url.openConnection();
+                System.out.println("String inside dobackground= "+params[0]);
+                sendPost(params[0]);
+                String response = readFromServlet();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            launchRegoSuccess();
+            finish();
+        }
+
+        private String readFromServlet() throws IOException {
+            BufferedReader stream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            return stream.readLine();
+        }
+
+        private void sendPost(String message) throws IOException {
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty( "charset", "utf-8");
+            connection.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
+            wr.write(message);
+            wr.flush();
+            wr.close();
+        }
     }
 }
